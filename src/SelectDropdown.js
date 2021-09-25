47a2078d0e65818ae9aa0178b893561314cba183
Mohsen Vahabzadeh <mohsen.v1980@yahoo.com>
2021-09-25 13:06:42 +0330

    Update SelectDropdown.js
    support for landscape in iOS added

--- src/SelectDropdown.js
+++ src/SelectDropdown.js
@@ -271,6 +271,7 @@ const SelectDropdown = (
     return (
       isVisible && (
         <Modal
+          supportedOrientations={['portrait', 'landscape']}
           animationType="none"
           transparent={true}
           statusBarTranslucent={
